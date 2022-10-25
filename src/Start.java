07996932f2d4f2f4b5d50e3b938a0b9e8f296556
Arthur Laquièze <arthur.laquieze@gmail.com>
2022-10-25 10:35:52 +0200

    add all different condition

--- src/Start.java
+++ src/Start.java
@@ -8,7 +8,15 @@ public class Start {
         Model model = new Model("nQueens");
         System.out.println(model.getName());
 
+        /**
+         * Le rang dans le tableau correspond à la ligne,
+         * la valeur d'une variable correspond à la colonne de la reine
+         * 
+         * Ce choix de modèle implique déjà une seule reine par ligne
+         */
         IntVar[] tableau = model.intVarArray("tableau", n, 1, n);
 
+        // ajout condition sur les colonnes, toutes différentes.
+        model.allDifferent(tableau);
     }
 }
