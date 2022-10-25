225c56f2efb858fe50d4d6133697c2338cb68fb2
Arthur Laquièze <arthur.laquieze@gmail.com>
2022-10-25 10:02:11 +0200

    init

---
+++ src/Start.java
@@ -0,0 +1,12 @@
+import org.chocosolver.solver.Model;
+
+public class Start {
+
+    public static void main(String[] args) {
+        Model model = new Model("A first model");
+        System.out.println(model.getName());
+    }
+
+
+
+}
