617b1f7715f94b9ae1c07bf418195ea04675cb41
Arthur Laquièze <arthur.laquieze@gmail.com>
2022-10-25 14:38:33 +0200

    add first variable

--- src/Pharmacy.java
+++ src/Pharmacy.java
@@ -2,9 +2,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.chocosolver.solver.Model;
+import org.chocosolver.solver.variables.BoolVar;
+import org.chocosolver.solver.variables.IntVar;
 
 public class Pharmacy {
     public static void pharmacyProblem() {
+        int nOrders = 100; // number of orders to simulate
+
+        // create processes
         Processus toPowderComprime = new Processus(20, 1, "Transformation to comprimé power");
         Processus toPowderGelule = new Processus(7, 1, "Transformation to gélule powder");
         Processus toPowderSachet = new Processus(14, 1, "Transformation to sachet powder");
@@ -13,10 +18,21 @@ public class Pharmacy {
         Processus powderToGelule = new Processus(5, 2, "Conditionnement vers gélule");
         Processus powderToSachet = new Processus(20, 2, "Conditionnement vers sachet");
 
-        List<Order> orders = generateOrders(100);
+        List<Processus> processes = new ArrayList<Processus>();
+        processes.add(toPowderComprime);
+        processes.add(toPowderGelule);
+        processes.add(toPowderSachet);
+        processes.add(powderToComprime);
+        processes.add(powderToGelule);
+        processes.add(powderToSachet);
+        List<Order> orders = generateOrders(nOrders);
 
         Model model = new Model("Pharmacy");
 
+        // ordre de traitement des commandes
+        IntVar[] processingOrder = model.intVarArray("Processing order", nOrders, 1, nOrders);
+        // list process occupé ou non (boolean)
+        BoolVar[] occupied = model.boolVarArray("Occupied processes", processes.size());
     }
 
     public static List<Order> generateOrders(int n) {
