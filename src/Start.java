0a6baa6277079a6dcd5b9dadd2ccd0c556212859
Alexandre OSTY <alexandre.osty@orange.fr>
2022-10-25 10:18:09 +0200

    add intvar

--- src/Start.java
+++ src/Start.java
@@ -3,7 +3,9 @@ import org.chocosolver.solver.Model;
 public class Start {
 
     public static void main(String[] args) {
+        int n = 10; 
         Model model = new Model("nQueens");
         System.out.println(model.getName());
+        model.intVar("tableau", 1, n);
     }
 }
