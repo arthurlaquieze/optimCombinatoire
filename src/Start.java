1393b6dea9ebfc7a16ec8e1bce07e7041d236767
Arthur Laquièze <arthur.laquieze@gmail.com>
2022-10-25 10:27:32 +0200

    question 2 début

--- src/Start.java
+++ src/Start.java
@@ -1,11 +1,14 @@
 import org.chocosolver.solver.Model;
+import org.chocosolver.solver.variables.IntVar;
 
 public class Start {
 
     public static void main(String[] args) {
-        int n = 10; 
+        int n = 10;
         Model model = new Model("nQueens");
         System.out.println(model.getName());
-        model.intVar("tableau", 1, n);
+
+        IntVar[] tableau = model.intVarArray("tableau", n, 1, n);
+
     }
 }
